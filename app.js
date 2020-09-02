12e300b245acd91ef586c742b1ee731ba4efefea
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-09-02 18:55:25 -0500

    add the pasword check

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 inicio();
+
 function inicio(){
-    console.log("New User");
-    console.log("Password");
+    
+    console.log("Welcome to the cash machine");
+   
+    password(prompt("clave"));
+
+
 }
 
-
-
-function user(){
-
+function users(){
+    return [
+        { nombre: "Juan", id: 1018407125, password: 2504 },
+        { nombre: "Camilo", id: 44445555, password: 1234 },
+        { nombre: "Estefanía", id: 1018443125, password: 2003 }
+    ]
 }
+
+function password(pass){
+    
+    const num = users().some( user =>{
+        return user.password == pass;
+    })
+
+    num ? console.log("next") :  password(prompt("nuevamente"));
+  }
