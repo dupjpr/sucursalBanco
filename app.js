25bb4409d0f6ea8d124489527c47bf57388b0f6d
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-09-02 22:19:36 -0500

    add the user format info

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 inicio();
 
+
 function inicio(){
-    
     console.log("Welcome to the cash machine");
-   
     password(prompt("clave"));
-
-
 }
+
+
+
 
 function users(){
     return [
-        { accountNumber: 12345678, nombre: "Juan", id: 1018407125, password: 2504 },
-        { accountNumber: 12345676, nombre: "Camilo", id: 44445555, password: 1234 },
-        { accountNumber: 12345679, nombre: "Estefanía", id: 1018443125, password: 2003 }
+        { saldo: 5000, accountNumber: 12345678, nombre: "Juan", id: 1018407125, password: 2504 },
+        { saldo: 10000, accountNumber: 12345676, nombre: "Camilo", id: 44445555, password: 1234 },
+        { saldo: 0, accountNumber: 12345679, nombre: "Estefanía", id: 1018443125, password: 2003 }
     ]
 }
 
-function password(pass){
-    
-    const num = users().some( user =>{
-        return user.password == pass;
-    })
+function userInformation(pass){
+    let userInfo;
+    users().forEach((user) =>{ user.password == pass ? userInfo = user : null; })
+    return userInfo;
+}
 
-    num ? console.log("next") : password(prompt("nuevamente"));
-  }
+function password(pass){
+    const num = users().some( user => {return user.password == pass;} )
+    num ? infoFormat(userInformation(pass)) : password(prompt("nuevamente"));
+}
+
+function infoFormat(userInfo) {
+
+    console.log(`Welcome to the bank GMLO ${userInfo.nombre}`);
+    console.log(`Your banck account is: ${userInfo.accountNumber}`);
+    console.log(`------------------`);
+    console.log(`Tu saldo actual es: ${userInfo.saldo}`);  
+    console.log(`Si quiere retirar marque 1\nSi desea consignar marque 2 \nSi desea transferir marque 3`);  
+    
+
+}
+
+
+
+
+
